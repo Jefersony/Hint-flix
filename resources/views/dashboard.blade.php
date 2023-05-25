7e28a26dcb3d312eeb9b31429ebb1e98225b154f
Jefersony <jeferson_rholanda@yahoo.com.br>
2023-05-25 00:45:26 -0300

    Adiciona template de formulário ao Dashboard com campos de entrada de dados para buscar filmes no sistema.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,36 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Insira os dados para buscar sugestões de filmes:") }}
+                    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                        <p></p>   
+                        <form method="POST" action="">
+                            @csrf
+                            @method('post')
+                            <label>Título: </label>
+                            <input type="text" name="titulo" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Ano Inicial: </label>
+                            <input type="text" name="anoInicial" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Ano Final: </label>
+                            <input type="text" name="anoFinal" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Gênero: </label>
+                            <input type="text" name="genero" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Estúdio: </label>
+                            <input type="text" name="estudio" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Diretor: </label>
+                            <input type="text" name="diretor" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <label>Elenco: </label>
+                            <br>
+                            <textarea 
+                                name="elenco" 
+                                placeholder="{{ __('Digite os nomes dos atores separando-os por vírgula.') }}"
+                                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                            <br>
+                            <x-input-error :messages="$errors->get('message')"
+                                class="mt-2" />
+                            <x-primary-button class="mt-4">{{ __('Buscar') }} </x-primary-button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
