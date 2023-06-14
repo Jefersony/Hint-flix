e5b0ceeaa3fbb1fd352698dfcc483d827ff16efe
Jefersony <jeferson_rholanda@yahoo.com.br>
2023-06-14 05:36:18 -0300

    Adiciona estilo à página Dashboard e modifica alguns atributos do menu de navegação do site.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,10 @@
+<head>
+    <title>Sugestões</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="<?php echo asset('css/homepage.css')?>" type="text/css">
+</head>
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,24 +16,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Insira os dados para buscar sugestões de filmes:") }}
+                    <p>
+                    </p>
                     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-                        <p></p>   
+                        <p>{{ __("Preencha qualquer um dos campos de dados de acordo com o que você deseja assistir, depois clique no botão -buscar- para receber sugestões de filmes:") }}</p>   
+                        <br><br>
                         <form method="GET" action="{{ route( 'trazer-filmes' ) }}">
                             @csrf
                             @method('get')
                             <label>Título: </label>
-                            <input type="text" name="titulo" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="titulo" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Lembra do título do filme que você procura?') }}"> <br>
                             <label>Ano Inicial: </label>
-                            <input type="text" name="anoInicial" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="anoInicial" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Procurar filmes a partir de qual ano?') }}"> <br>
                             <label>Ano Final: </label>
-                            <input type="text" name="anoFinal" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="anoFinal" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Não te interessam filmes a partir de qual ano?') }}"> <br>
                             <label>Gênero: </label>
-                            <input type="text" name="genero" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="genero" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Que tipo de filme você procura?') }}"> <br>
                             <label>Estúdio: </label>
-                            <input type="text" name="estudio" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="estudio" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Algum distribuidor de filmes preferido?') }}"> <br>
                             <label>Diretor: </label>
-                            <input type="text" name="diretor" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"> <br>
+                            <input type="text" name="diretor" class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                placeholder="{{ __('Você procura obras de um diretor específico?') }}"> <br>
                             <label>Elenco: </label>
                             <br>
                             <textarea 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,13 @@
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Entrar</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            <span class="font-semibold text-gray-100 hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">  |  </span>
+                            <a href="{{ route('register') }}" class="font-semibold text-gray-300 hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cadastrar</a>
                         @endif
                     @endauth
                 </div>
@@ -40,7 +41,7 @@
                         <div class="col mb-5 home-content--desc">
                             <div class="mb-3">
                                 <h2 class=" display-1 relative bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-black"
-                                > H i n t-f l i x </h2>
+                                > H i n t f l i x </h2>
                             </div>
                             <div class="button d-flex justify-content-center">
                                 <button type="button" class="btn button-canva">Receba dicas de filmes</button>
