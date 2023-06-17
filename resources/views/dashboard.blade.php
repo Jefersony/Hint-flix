8204f228ec200a6cd4544dbff661ee21a86e238e
Jefersony <jeferson_rholanda@yahoo.com.br>
2023-06-17 07:59:06 -0300

    Traduz ou altera o título das páginas e alguns rótulos de formulários.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Sugestões</title>
+    <title>Painel</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="<?php echo asset('css/homepage.css')?>" type="text/css">
 </head>
@@ -8,7 +8,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Painel De Busca') }}
         </h2>
     </x-slot>
 
